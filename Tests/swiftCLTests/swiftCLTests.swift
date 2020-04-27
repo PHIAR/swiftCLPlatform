322fb5ed1b4d02fddb7d960b8a54be1255ad00b6
Aaditya Chandrasekhar <aaditya.c@phiar.net>
2020-04-27 03:43:25 -0700

    Refresh swiftCLTest for OpenCL squareing values.

--- Tests/swiftCLTests/swiftCLTests.swift
+++ Tests/swiftCLTests/swiftCLTests.swift
@@ -2,18 +2,24 @@ import COpenCL
 import Foundation
 import XCTest
 
-private let DATA_SIZE = 1024
+private let SWIFTCL_ENABLE_LOG = false
 
 internal final class swiftCLTests: XCTestCase {
-    func testExample() {
-        let source = ("""
-            __kernel void square(__global float *input, __global float *output, const unsigned int count) {
-                int i = get_global_id(0);
+    private static let maxIterations = 2048
 
-                if (i < count) {
-                    output[i] = input[i] * input[i];
-                }
+    func testSquareValues() {
+        let source = ("""
+        __kernel void
+        square(__global float *input,
+               __global float *output,
+               unsigned int const count)
+        {
+            int i = get_global_id(0);
+
+            if (i < count) {
+                output[i] = input[i] * input[i];
             }
+        }
         """).utf8CString
 
         var err = cl_int(0)
@@ -33,46 +39,58 @@ internal final class swiftCLTests: XCTestCase {
 
         clBuildProgram(program, 0, nil, nil, nil, nil)
 
+        let maxIterations = swiftCLTests.maxIterations
         let kernel = clCreateKernel(program, "square", &err)!
-        var input = clCreateBuffer(context,  cl_mem_flags(CL_MEM_READ_ONLY),  MemoryLayout <Float>.size * DATA_SIZE, nil, nil)!
-        var output = clCreateBuffer(context, cl_mem_flags(CL_MEM_WRITE_ONLY), MemoryLayout <Float>.size * DATA_SIZE, nil, nil)!
-        var data: [Float] = []
+        var input = clCreateBuffer(context,  cl_mem_flags(CL_MEM_READ_ONLY),  MemoryLayout <Float>.size * maxIterations, nil, nil)!
+        var output = clCreateBuffer(context, cl_mem_flags(CL_MEM_WRITE_ONLY), MemoryLayout <Float>.size * maxIterations, nil, nil)!
 
-        for i in 0..<DATA_SIZE {
-            data.append(Float(i))
-        }
+        for iteration in 1...maxIterations {
+            var count = iteration
+            var data = Array(repeating: Float(1.0),
+                             count: count)
 
-        err = clEnqueueWriteBuffer(commands, input, cl_bool(CL_TRUE), 0, MemoryLayout <Float>.size * DATA_SIZE, data, 0, nil, nil)
-        clSetKernelArg(kernel, 0, MemoryLayout <cl_mem>.size, &input)
-        clSetKernelArg(kernel, 1, MemoryLayout <cl_mem>.size, &output)
-
-        var count = DATA_SIZE
-
-        clSetKernelArg(kernel, 2, MemoryLayout <UInt32>.size, &count);
-
-        var local = size_t(0)
-
-        clGetKernelWorkGroupInfo(kernel, device_id, cl_kernel_work_group_info(CL_KERNEL_WORK_GROUP_SIZE), MemoryLayout <size_t>.size, &local, nil)
-
-        var global = count
-
-        clEnqueueNDRangeKernel(commands, kernel, 1, nil, &global, &local, 0, nil, nil)
-        clFinish(commands)
-
-        var results = Array(repeating: Float(0.0),
-                            count: DATA_SIZE)
-
-        clEnqueueReadBuffer(commands, output, cl_bool(CL_TRUE), 0, MemoryLayout <Float>.size * count, &results[0], 0, nil, nil)
-
-        var correct = 0
-
-        for i in 0..<count {
-            if results[i] == (data[i] * data[i]) {
-                correct += 1
+            for i in 0..<count {
+                data[i] = Float(min(Int(sqrt(Float(65504))), i * iteration))
             }
+
+            err = clEnqueueWriteBuffer(commands, input, cl_bool(CL_TRUE), 0, MemoryLayout <Float>.size * count, data, 0, nil, nil)
+
+            clSetKernelArg(kernel, 0, MemoryLayout <cl_mem>.size, &input)
+            clSetKernelArg(kernel, 1, MemoryLayout <cl_mem>.size, &output)
+            clSetKernelArg(kernel, 2, MemoryLayout <UInt32>.size, &count);
+
+            var local = size_t(0)
+
+            clGetKernelWorkGroupInfo(kernel, device_id, cl_kernel_work_group_info(CL_KERNEL_WORK_GROUP_SIZE), MemoryLayout <size_t>.size, &local, nil)
+
+            var global = count
+
+            clEnqueueNDRangeKernel(commands, kernel, 1, nil, &global, &local, 0, nil, nil)
+            clFinish(commands)
+
+            var results = Array(repeating: Float(0.0),
+                                count: count)
+
+            clEnqueueReadBuffer(commands, output, cl_bool(CL_TRUE), 0, MemoryLayout <Float>.size * count, &results[0], 0, nil, nil)
+
+            var correct = 0
+            let expected = data.map { $0 * $0 }
+
+            for i in 0..<count {
+                if results[i] == expected[i] {
+                    correct += 1
+                }
+            }
+
+            if SWIFTCL_ENABLE_LOG {
+                print("Expected: \(expected)")
+                print("Got: \(results)")
+            }
+
+            print("Iteration \(iteration) computed '\(correct)/\(count)' correct values!")
+            XCTAssertEqual(correct, count)
         }
 
-        print("Computed '\(correct)/\(count)' correct values!\n")
         clReleaseMemObject(input)
         clReleaseMemObject(output)
         clReleaseProgram(program)
@@ -82,7 +100,7 @@ internal final class swiftCLTests: XCTestCase {
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testSquareValues", testSquareValues),
     ]
 }
 
